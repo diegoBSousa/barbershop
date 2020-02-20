7586874ace98301bb44d9dc9000129e5dd370164
Diego Sousa <diego@diegosousa.com.br>
2020-02-20 15:56:45 -0300

    Add Appointment model

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,12 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 const models = [
   User,
-  File
+  File,
+  Appointment
 ];
 
 class Database{
@@ -15,7 +17,7 @@ class Database{
 
   init(){
     this.connection = new Sequelize(databaseConfig);
-    
+
     models
     .map(model => model.init(this.connection))
     .map(model => model.associate && model.associate(this.connection.models));

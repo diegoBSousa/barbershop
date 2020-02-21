9c264f3211da51731b6db6e833196accf54dfddf
Diego Sousa <diego@diegosousa.com.br>
2020-02-21 11:37:23 -0300

    Persist appointment in database

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,37 @@
 import Appointment from '../models/Appointment';
+import * as Yup from 'yup';
+import User from '../models/User'; 
 
 class AppointmentController{
   async store(req, res){
-    return res.json(req);
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required()
+    });
+
+    if(!(schema.isValid(req.body))){
+      return res.status(400).json({ error: "Validation has failed"});
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({
+      where: {
+        user_id: provider_id,
+        provider: true
+      }
+    });
+
+    if(!isProvider){
+      return res.status(401).json({ error: "That provider was not found"});
+    }
+console.log(`userId: ${req.userId}`);
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date
+    });
+    return res.json(appointment);
   }
 }
 

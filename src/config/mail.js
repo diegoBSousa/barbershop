50f66d3e7c1473ff305ce2353d44af84e245161d
Diego Sousa <diego@diegosousa.com.br>
2020-02-27 09:56:03 -0300

    Add mail configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'ca5a93b98b51b2',
+    pass: '79da3fd128bfb9',
+  },
+  default: {
+    from: 'Barber Shop Crew <noreply@barbershop.com>'
+  }
+};

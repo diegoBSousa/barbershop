3a4b17330d87a34adf722a3fdc0be7f6cc0a7e43
Diego Sousa <diego@diegosousa.com.br>
2020-08-22 20:21:28 -0300

    Configure Sentry handlers

--- src/app.js
+++ src/app.js
@@ -10,11 +10,13 @@ class App {
   constructor(){
     this.server = express();
     Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares(){
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files/',
@@ -24,6 +26,7 @@ class App {
 
   routes(){
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
 }
